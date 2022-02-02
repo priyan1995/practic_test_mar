48eb618dd6aa474fb5afd95063314d9289c36c82
priyan <45042030+priyan1995@users.noreply.github.com>
2022-02-02 16:20:50 +0530

    news section development ongoing

--- wp-content/themes/EENG/front-page.php
+++ wp-content/themes/EENG/front-page.php
@@ -52,4 +52,127 @@ get_header();
     </div>
 </section>
 
+
+<!-- latest news section -->
+<section class="pos-rel pd-padd pd-latest-news-sec pd-dbl-p-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <h3>Latest Public Job Posts</h3>
+
+                <a href="#">
+                    <div class="pd-news-card">
+                        <div>
+                            <h4>Assistant Stopkeeper</h4>
+                            <p>JSW Apparels (Pvt) Ltd</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="pd-news-card">
+                        <div>
+                            <h4>Assistant Stopkeeper</h4>
+                            <p>JSW Apparels (Pvt) Ltd</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="pd-news-card">
+                        <div>
+                            <h4>Senior Software Engineer (.Net) - Remote</h4>
+                            <p>Champ IT Solutions</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="pd-news-card">
+                        <div>
+                            <h4>Excecutive - Customer Services</h4>
+                            <p>Genesis Software</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="pd-news-card">
+                        <div>
+                            <h4>Senior Brand Excecutive</h4>
+                            <p>Wallspan (Pvt) Ltd</p>
+                        </div>
+                    </div>
+                </a>
+
+
+
+            </div>
+            <div class="col-lg-7">
+                <h3>Latest Common Job News</h3>
+                <div class="row">
+                    <div class="col-12">
+
+                    </div>
+
+                    <div class="col-lg-6">
+                        <a href="#">
+                            <div class="pd-news-card pd-sm-news-card">
+                            <img src="<?php echo bloginfo('template_url') ?>/assets/img/dumJb.jpg" >
+                                <div>
+                                    <h4>Face online interview</h4>
+                                    <p>Genesis Software</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col-lg-6">
+                        <a href="#">
+                            <div class="pd-news-card pd-sm-news-card">
+                            <img src="<?php echo bloginfo('template_url') ?>/assets/img/dumJb.jpg" >
+                                <div>
+                                    <h4>Face online interview</h4>
+                                    <p>Genesis Software</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <a href="#">
+                            <div class="pd-news-card pd-sm-news-card">
+                            <img src="<?php echo bloginfo('template_url') ?>/assets/img/dumJb.jpg" >
+                                <div>
+                                    <h4>Face online interview</h4>
+                                    <p>Genesis Software</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <a href="#">
+                            <div class="pd-news-card pd-sm-news-card">
+                            <img src="<?php echo bloginfo('template_url') ?>/assets/img/dumJb.jpg" >
+                                <div>
+                                    <h4>Face online interview</h4>
+                                    <p>Genesis Software</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
 <?php get_footer(); ?>
